116f71dd1a0e7eb5fe064eb26f7e669884b435f4
jonathanGar <jonathange87@gmail.com>
2019-03-28 14:04:40 -0500

    Se crea estructura de modelos y controladores para el modelo de cursos

--- proyecto_node_TDA/routes/cursos.js
+++ proyecto_node_TDA/routes/cursos.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var cursosController = require('../controllers/cursosController');
 
 router.get('/', (req, res, next) => {
   res.render('Cursos', {
@@ -16,10 +17,38 @@ router.get('/crear', (req, res, next) => {
 });
 
 router.post('/crear', (req, res, next) => {
-  //req.body.variable
+  let description;
+  let creado = false;
+  console.log("1", req.body.nombre, req.body.id, req.body.valor, req.body.descripcion);
+  if(req.body.nombre && req.body.id && req.body.valor && req.body.descripcion){
+    console.log("2");
+    let newCurso = {
+      nombre: req.body.nombre,
+      id: parseInt(req.body.id),
+      valor: parseFloat(req.body.valor),
+      descripcion: req.body.descripcion,
+      modalidad: req.body.modalidad,
+      intensidad: parseInt(req.body.intensidad),
+    }
+
+    if(cursosController.crear(newCurso)){
+      console.log("3");
+      description = "Curso creado exitosamente";
+      creado = true;      
+    }else{
+      console.log("4");
+      description = "Ocurrió un error al tratar de crear el curso";
+    }
+
+  }else{
+    console.log("5");
+    description = "Por favor revise los campos obligatorios";
+  }
+  
   res.render('cursos/crear', {
-    h1: 'Curso creado',
-    description: 'Se ha creado el curso exitosamente'
+    h1: 'Creación de cursos',
+    description: description,
+    creado: creado
   });
 });
 
